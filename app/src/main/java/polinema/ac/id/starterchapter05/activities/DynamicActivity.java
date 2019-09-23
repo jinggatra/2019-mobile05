5886d80ec936d602551a8ee77f4fa4b174a2f417
jinggatra <wmkbisa@gmail.com>
2019-09-23 14:52:32 +0700

    dynamic fragment

---
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
@@ -0,0 +1,23 @@
+package polinema.ac.id.starterchapter05.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import polinema.ac.id.starterchapter05.R;
+
+public class DynamicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dynamic);
+    }
+
+    public void handlerClickLoadRedFragment(View view) {
+    }
+
+    public void handlerClickLoadBlueFragment(View view) {
+    }
+}
